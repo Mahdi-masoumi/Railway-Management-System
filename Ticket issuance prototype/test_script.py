a46289f54e8956b258d720fbeca9c154c7bddb5b
mhfereydooni <mh.fereiduni@gmail.com>
2025-09-19 17:16:52 +0330

    Update test_script.py
    نوشتن کد کلاس سیستم بلیط که وظیفش :
    گرفتن شئ بلیط ،باز کردن فایل متنی،اضافه کردن  اطلاعات بلیط به فایل،درست کردن ظاهر پرینت و در نهایت نشان دادن پیام صدور موفقیت

--- Ticket issuance prototype/test_script.py
+++ Ticket issuance prototype/test_script.py
@@ -16,3 +16,12 @@ Price per Ticket : {self.price}
 Total Price : {total_price}
 Purchase Time : {self.purchase_time}
 """
+class TicketSystem:
+    def __init__(self, file_name="tickets.txt"):
+        self.file_name = file_name
+
+    def issue_ticket(self, ticket):
+        with open(self.file_name, "a", encoding="utf-8") as file:
+            file.write(ticket.get_ticket_info())
+            file.write("\n" + "-"*50 + "\n")
+        print("bilit ba movafaghiat sader va zhakhire shod")
