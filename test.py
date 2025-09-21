c7c51c50ed76a6e939ddd28e109bd1bf507f6056
Reyhaneh <reyhanehsafaei@gmail.com>
2025-09-21 23:30:16 +0330

    admin palone

---
+++ test.py
@@ -0,0 +1,21 @@
+employees_data=[]
+ADMIN_USERNAME="Admin_Train"
+ADMIN_PASSWORD="Pass_Train"
+
+def display_meassage(message , message_type="errore")
+    if message_type=="errore":
+        print("errore")
+    elif message_type=="success": 
+        print("success")
+    else:
+        print(f"\n{message}") 
+
+
+
+
+  
+
+
+
+
+                  
